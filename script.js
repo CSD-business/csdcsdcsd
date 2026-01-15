d300881d4fef88efab19e5f0c680ee5420654d21
CSD-business <143720980+CSD-business@users.noreply.github.com>
2026-01-15 12:47:49 -0500

    Total portfolio revamp

---
+++ script.js
@@ -0,0 +1,13 @@
+﻿function loadGame(container, url) {
+    container.innerHTML = `
+        <iframe 
+            src="${url}"
+            class="game-frame"
+            loading="lazy">
+        </iframe>
+
+        <a href="${url}" target="_blank" class="play-button">
+            Play Fullscreen
+        </a>
+    `;
+}
